b7f379906e688c2eafcdaa8ba3348869ae8d0e33
SCratORS <scrators@mail.ru>
2021-10-23 12:15:41 +0300

    Update FTPCommon.h

--- FTPCommon.h
+++ FTPCommon.h
@@ -8,12 +8,12 @@
 
 #ifdef ESP8266
 #include <PolledTimeout.h>
-using arduino::esp8266::polledTimeout::oneShotMs; // import the type to the local namespace
+using esp8266::polledTimeout::oneShotMs; // import the type to the local namespace
 #define BUFFERSIZE TCP_MSS
 #define PRINTu32 "lu"
 #elif defined ESP32
 #include "esp32compat/PolledTimeout.h"
-using arduino::esp8266::polledTimeout::oneShotMs;
+using esp8266::polledTimeout::oneShotMs;
 #define BUFFERSIZE CONFIG_TCP_MSS
 #define PRINTu32 "u"
 #endif
